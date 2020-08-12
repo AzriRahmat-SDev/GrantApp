26087a2f86f883d3e74e3b72764a8b37a202bb34
Azri Rahmat <AhCherLy@Muhammads-MacBook-Pro.local>
2020-08-12 17:06:36 +0800

    hello

--- src/components/FGPersonalDetails.js
+++ src/components/FGPersonalDetails.js
@@ -15,7 +15,7 @@ export class FGPersonalDetails extends Component {
 
    constructor(props){
         super(props);
-        console.log(this.props)  //to see if the props fr
+        console.log(this.props)  //to see if the props 
     } 
     continue = e => {
         e.preventDefault();
