c399047b4839db30fa075d2d95e93547f593cdd7
Azri Rahmat <AhCherLy@Muhammads-MacBook-Pro.local>
2020-11-11 12:57:18 +0800

    Updates

---
+++ precache-manifest.ae8ff2071b3e44b0c3d0beb24c2fcd3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351f06d8121928fb393f82e5c467f2ee",
+    "url": "/GrantApp/index.html"
+  },
+  {
+    "revision": "dd372c21921774afbfd3",
+    "url": "/GrantApp/static/css/main.039de262.chunk.css"
+  },
+  {
+    "revision": "b4a2cb7db3e38acb8914",
+    "url": "/GrantApp/static/js/2.3ce8fdfd.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/GrantApp/static/js/2.3ce8fdfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd372c21921774afbfd3",
+    "url": "/GrantApp/static/js/main.3350f354.chunk.js"
+  },
+  {
+    "revision": "6dbc5f3f298dd450d444",
+    "url": "/GrantApp/static/js/runtime-main.ec5e8b82.js"
+  }
+]);
